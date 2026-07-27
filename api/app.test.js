f0504d6a46961479f6172dcba5fa5ed395dd03ee
vagrant <vagrant@control>
2026-07-27 16:42:24 +0000

    breaking the code

--- api/app.test.js
+++ api/app.test.js
@@ -4,5 +4,5 @@ const app = require("./app");
 test("GET /healthz returns ok", async () => {
   const res = await request(app).get("/healthz");
   expect(res.statusCode).toBe(200);
-  expect(res.body.status).toBe("ok");
+  expect(res.body.status).toBe("notok");
 });
